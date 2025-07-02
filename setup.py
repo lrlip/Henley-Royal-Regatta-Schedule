20681c31b757c7a4e505a52ae80327beefd1ced7
lrlip <51962580+lrlip@users.noreply.github.com>
2025-07-02 21:55:54 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,4 @@ setup(
         "pyyaml"
     ],
     python_requires=">=3.10.0",
-    description="""This script fetches and displays the race timetable for the Henley Royal Regatta.
-    It allows users to search for specific crews and displays the race schedule in both GB and local 
-    times based on the specified GMT offset. Run this script after the draw is published to get the latest results.""")
+    description="""This script fetches and displays the race timetable for the Henley Royal Regatta. It allows users to search for specific crews and displays the race schedule in both GB and local  times based on the specified GMT offset. Run this script after the draw is published to get the latest results.""")
